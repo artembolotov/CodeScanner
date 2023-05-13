9a51b67209406bea6079a1e16aa4105e64589cbd
Artem Bolotov <teeemon@gmail.com>
2023-05-13 07:47:32 +0300

    Added FileManager with clearTMPDir method

---
+++ Sources/CodeScanner/FileManager.swift
@@ -0,0 +1,30 @@
+//
+//  FileManager.swift
+//  TwinKey
+//
+//  Created by artembolotov on 13.05.2023.
+//
+
+import Foundation
+
+extension FileManager {
+    static func clearTmpDir() {
+        let fileManager = self.default
+        let tempDirURL = fileManager.temporaryDirectory
+
+        do {
+            let directoryContents = try fileManager.contentsOfDirectory(at: tempDirURL, includingPropertiesForKeys: .none)
+
+            for file in directoryContents {
+                do {
+                    try fileManager.removeItem(at: file)
+                    print("Deleted file \(file.absoluteString)")
+                } catch {
+                    print("Error while deleting file. \(error.localizedDescription)")
+                }
+            }
+        } catch {
+            print("Error! \(error.localizedDescription)")
+        }
+    }
+}
